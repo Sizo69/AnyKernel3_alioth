1a6213b2883a8529e107d69421b6e8c6fa58b38c
Kaneda Shotaro <130251738+Sizo69@users.noreply.github.com>
2024-09-16 18:26:37 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,18 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Redmi 5 Plus kernel
+kernel.string=Skyline-Proton Kernel
 do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=vince
-supported.versions=
+device.name1=vayu
+device.name2=bhima
+device.name3=
+device.name4=
+device.name5=
+supported.versions=11.0-14.0
 supported.patchlevels=
 '; } # end properties
 
@@ -31,8 +35,24 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel boot install
 dump_boot;
 write_boot;
 ## end boot install
+
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
+
